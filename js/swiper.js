1a730e6701fa2fdc1814e8c0042af94f783732a8
kms12191 <ste_reo@naver.com>
2026-03-19 18:01:27 +0900

    260319_2

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,16 @@
 // #silder swiper
-var silder_swiper = new Swiper(".silderSwiper", {
+var slider_swiper = new Swiper(".sliderSwiper", {
     rewind: true,
     navigation: {
-        nextEl: "#silder .swiper-button-next",
-        prevEl: "#silder .swiper-button-prev",
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
     },
     autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
     pagination: {
-        el: "#silder .swiper-pagination",
+        el: "#slider .swiper-pagination",
     },
 });
 
@@ -22,11 +22,10 @@ var new_swiper = new Swiper(".newSwiper", {
     spaceBetween: 20,
     pagination: {
         el: "#new .swiper-pagination",
-        clickable: true,
+        type: "fraction",
     },
     navigation: {
         nextEl: "#new .swiper-button-next",
         prevEl: "#new .swiper-button-prev",
-    },
-    slidesPerGroup: 3
+    }
 });
