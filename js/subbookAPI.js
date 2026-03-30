dc26bce02dd0688179af1075d09f23f361336248
kms12191 <ste_reo@naver.com>
2026-03-30 17:55:14 +0900

    260330

--- js/subbookAPI.js
+++ js/subbookAPI.js
@@ -52,9 +52,7 @@ async function mainBook() {
                 saleNum[2].textContent += (sale_price + 3000).toLocaleString() + "원";
                 
 
-                contextBox.innerHTML = `<p>${authors[0]}</p>
-                <br>
-                <p>${contents}</p>
+                contextBox.innerHTML = `
                 <img src="../img/bookmain.jpg" alt="책 메인이미지">
                 `
             } catch (error) {
@@ -65,6 +63,7 @@ async function mainBook() {
         mainBook();
 
         //메모장으로 sub 텍스트 가져오기, 서버에 올려야 보임
+        //기본정보
         document.addEventListener("DOMContentLoaded", async function () {
             try {
                 const response = await fetch("./txt/sub_txt1.txt");
@@ -77,9 +76,32 @@ async function mainBook() {
                 console.error("There was a problem with the fetch operation:", error);
             }
         });
-
-
-
+        //배송/교환/반품
+        document.addEventListener("DOMContentLoaded", async function () {
+            try {
+                const response = await fetch("./txt/sub_txt2.txt");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.text();
+                document.getElementById("delivery").innerHTML = data;
+            } catch (error) {
+                console.error("There was a problem with the fetch operation:", error);
+            }
+        });
+        //분철안내
+        document.addEventListener("DOMContentLoaded", async function () {
+            try {
+                const response = await fetch("./txt/sub_txt3.txt");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.text();
+                document.getElementById("info").innerHTML = data;
+            } catch (error) {
+                console.error("There was a problem with the fetch operation:", error);
+            }
+        });
 
 
 // tabs
@@ -104,7 +126,7 @@ tabMenu.forEach((tm, i) => {
     // 탭에 해당하는 리스트 보이고, 나머지는 숨기기
     tabContent.forEach((tc, j) => {
       tc.style.display = (i === j) ? 'flex' : 'none';
-      tab.style.height = '350px';
+      tab.style.height = '360px';
       tc.style.height = '295px';
       btns[i].innerText = '더보기'
     });
@@ -115,13 +137,13 @@ tabMenu.forEach((tm, i) => {
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
     if (btn.textContent == '더보기') {
-      tab.style.height = '2780px';
+      tab.style.height = '6655px';
       tabContent.forEach(tc => {
-        tc.style.height = '2728px';
+        tc.style.height = '6600px';
       });
       btn.innerText = '접기'
     } else {
-      tab.style.height = '350px';
+      tab.style.height = '360px';
       tabContent.forEach(tc => {
         tc.style.height = '300px';
       });
@@ -131,6 +153,58 @@ btns.forEach(btn => {
 });       
 
 
+
+
+async function newBookData() {
+    const params = new URLSearchParams({
+        target: "author",
+        query: "최태성",
+        size: 10
+    });
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: "KakaoAK 075f6cd693c270228fc9fc2c238a73cd"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll("#new .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // 요소 생성 및 추가
+            box.innerHTML = `<img src="${data.documents[i].thumbnail}">
+            <div class='slider_content'>
+                <h3>${data.documents[i].title}</h3>
+                <h4>${data.documents[i].authors}·${data.documents[i].publisher}</h4>
+                <p>${data.documents[i].sale_price.toLocaleString()}원</p>
+            </div>
+                    `
+        });
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+
+
+newBookData();
 
 //swiper//
 var swiper = new Swiper(".newSwiper", {
